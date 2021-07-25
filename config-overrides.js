96027aa7803746fc7d32edda907fecb1d942e44c
lenam <lenam.opentechiz@gmail.com>
2021-07-25 18:17:53 +0700

    add module path @utils

--- config-overrides.js
+++ config-overrides.js
@@ -4,5 +4,7 @@ const path = require('path');
 module.exports = override(
   addWebpackAlias({
     '@components': path.resolve(__dirname, 'src/components'),
+    '@modules': path.resolve(__dirname, 'src/modules'),
+    '@utils': path.resolve(__dirname, 'src/utils'),
   }),
 );
