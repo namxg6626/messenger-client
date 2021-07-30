e431dfe56dc9b2a56dc36cd2876e2b10680c585d
lenam <lenam.opentechiz@gmail.com>
2021-07-30 10:23:15 +0700

    add render list of conversations

--- src/layout/ChatDesktop/index.js
+++ src/layout/ChatDesktop/index.js
@@ -2,10 +2,24 @@ import { Layout, Typography, Row, Col } from 'antd';
 import { StyledSider } from './styled';
 import { ChatCard, BaseInput } from '@components/index';
 import { Outlet } from 'react-router-dom';
+import * as __mock__ from 'src/__mock__';
 
 import styles from './styles.module.scss';
 
 export function ChatDesktop() {
+  const _renderMockConversations = () => {
+    return __mock__.conversations.map((conversation) => (
+      <Col span={24}>
+        <ChatCard
+          from={conversation.from}
+          avatar={conversation.avatar}
+          lastMessage={conversation.lastMessage}
+          onClick={() => console.log(`conversation._id`, conversation._id)}
+        />
+      </Col>
+    ));
+  };
+
   return (
     <Layout>
       <StyledSider>
@@ -18,21 +32,7 @@ export function ChatDesktop() {
               placeholder='Tìm kiếm người dùng hoặc nhóm..'
             />
           </Col>
-          <Col span={24}>
-            <ChatCard />
-          </Col>
-          <Col span={24}>
-            <ChatCard />
-          </Col>
-          <Col span={24}>
-            <ChatCard />
-          </Col>
-          <Col span={24}>
-            <ChatCard />
-          </Col>
-          <Col span={24}>
-            <ChatCard />
-          </Col>
+          {_renderMockConversations()}
         </Row>
       </StyledSider>
       <Layout>
