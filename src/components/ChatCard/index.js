26ef09129380dc8a07f9bc8821100d274c732598
lenam <lenam.opentechiz@gmail.com>
2021-07-30 10:22:13 +0700

    add ChatCard props

--- src/components/ChatCard/index.js
+++ src/components/ChatCard/index.js
@@ -2,23 +2,35 @@
 import { Row, Col, Avatar, Typography } from 'antd';
 import { generateRandomColor } from '../../utils/generator';
 import styles from './styles.module.scss';
+import PropTypes from 'prop-types';
+
+import { getShortName } from '@utils/getters';
 
 const { Paragraph, Text } = Typography;
 
-export function ChatCard() {
+export function ChatCard({ lastMessage = '', avatar = '', from = '', onClick = () => null }) {
   return (
     <Row gutter={16} className={styles.chatCard} align='middle'>
       <Col span={5}>
-        <Avatar size='large' style={{ backgroundColor: generateRandomColor() }}>
-          K
+        <Avatar src={avatar} size='large' style={{ backgroundColor: generateRandomColor() }}>
+          {getShortName(from).toUpperCase()}
         </Avatar>
       </Col>
       <Col span={19}>
-        <Paragraph className={styles.chatName}>User name</Paragraph>
+        <Paragraph className={styles.chatName} ellipsis>
+          {from}
+        </Paragraph>
         <Text className={styles.lastMessage} ellipsis>
-          lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+          {lastMessage}
         </Text>
       </Col>
     </Row>
   );
 }
+
+ChatCard.propTypes = {
+  lastMessage: PropTypes.string,
+  avatar: PropTypes.string,
+  onClick: PropTypes.func,
+  from: PropTypes.string,
+};
